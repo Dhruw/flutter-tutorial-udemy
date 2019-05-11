9868e0167b9d43bfc158e5d73c9bd5bdc1e5132d
Divyanshu Dhruw <divyanshudhruw@gmail.com>
2019-05-11 17:58:04 +0530

    Successfully implemented List Array and setState method

--- lib/main.dart
+++ lib/main.dart
@@ -4,32 +4,49 @@ main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  build(context) {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _bikes = ["Biker"];
+
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Hello Divyanshu"),
         ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                child: Text("My Button"),
-                onPressed: () => {},
-              ),
-            ),
-            Card(
-              child: Column(
-                children: <Widget>[
-                  Text("Hello Body"),
-                  Image.asset('assets/images/Dominar.jpg')
-                ],
-              ),
-            ),
-          ],
-        ),
+        body: Column(children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: RaisedButton(
+                child: Text("Add Bikes"),
+                onPressed: () {
+                  setState(() {
+                    _bikes.add("Hamara Bajaj");
+                    print(_bikes);
+                  });
+                }),
+          ),
+          Column(
+            children: _bikes
+                .map(
+                  (bikeName) => Card(
+                        child: Column(
+                          children: <Widget>[
+                            Text(bikeName),
+                            Image.asset('assets/images/Dominar.jpg')
+                          ],
+                        ),
+                      ),
+                )
+                .toList(),
+          ),
+        ]),
       ),
     );
   }
